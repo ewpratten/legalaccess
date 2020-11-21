8d565336db2b2e735fd0ccaae6fa1a9f4d68157f
Evan Pratten <ewpratten@gmail.com>
2020-11-21 14:08:02 -0500

    Accessor tests

--- src/test/java/ca/retrylife/legalaccess/AccessorTest.java
+++ src/test/java/ca/retrylife/legalaccess/AccessorTest.java
@@ -1,5 +1,6 @@
 package ca.retrylife.legalaccess;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,9 +10,10 @@ public class AccessorTest {
     private class TestClass {
         private boolean testValueTrue = true;
     }
-    
+
     @Test
-    public void testProperAccess() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    public void testProperAccess()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
 
         // Access the test value
         TestClass clazz = new TestClass();
@@ -19,6 +21,24 @@ public class AccessorTest {
 
         // Fetch the value
         assertTrue("Value", a.get());
-        
+
+    }
+
+    @Test
+    public void testInvalidAccess() {
+
+        // Access the test value
+        TestClass clazz = new TestClass();
+        Accessor<Boolean> a = null;
+
+        try {
+            a = new Accessor<>("testValueInvalid", clazz, Boolean.class);
+        } catch (NoSuchFieldException | SecurityException e) {
+            assertTrue("Caught exception", true);
+            return;
+        }
+
+        // Fetch the value
+        assertNull("Value", a.getOrNull());
     }
 }
